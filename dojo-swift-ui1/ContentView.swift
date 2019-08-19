ff8a0406adcc028ddeaa2affa78887536ee5ffb5
Dennis <dennis.rodrigues@aisdigital.com.br>
2019-08-19 15:15:22 -0300

    Initial Commit

---
+++ dojo-swift-ui1/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  dojo-swift-ui1
+//
+//  Created by Dennis Merli on 19/08/19.
+//  Copyright © 2019 Dennis Merli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ dojo-swift-ui1Tests/dojo_swift_ui1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  dojo_swift_ui1Tests.swift
+//  dojo-swift-ui1Tests
+//
+//  Created by Dennis Merli on 19/08/19.
+//  Copyright © 2019 Dennis Merli. All rights reserved.
+//
+
+import XCTest
+@testable import dojo_swift_ui1
+
+class dojo_swift_ui1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
