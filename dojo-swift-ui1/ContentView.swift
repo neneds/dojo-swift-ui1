f94ec9fc0b655f55fe1d8feb88d9b952fcfff2a1
Dennis Merli <neneds@gmail.com>
2019-08-19 22:55:13 -0300

    creating ui elements

--- dojo-swift-ui1/ContentView.swift
+++ dojo-swift-ui1/ContentView.swift
@@ -9,15 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var email: String
+    @State var password: String
+    
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Rectangle()
+                .foregroundColor(.clear)
+                .background(LinearGradient(gradient: Gradient(colors: [Color("pinkff00cc"), Color("blue333399")]), startPoint: .leading, endPoint: .trailing))
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Group {
+                    TextField("Email", text: $email)
+                    SecureField("Password", text: $password)
+                    
+                }.frame(width: UIScreen.main.bounds.size.width - 80, height: 41, alignment: .center)
+                    .textContentType(.emailAddress)
+                    .background(Color.black.opacity(0.6))
+                    .cornerRadius(10)
+                    .multilineTextAlignment(.center)
+                .zIndex(1)
+            }
+        }
     }
 }
 
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(email: "", password: "")
     }
 }
 #endif
